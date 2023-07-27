171b8e515508ae3b7db7341c85fb1e6a69e52caf
ipu <uwu@ipu.moe>
2023-07-27 17:54:21 +0300

    feat: add logger

--- bot.py
+++ bot.py
@@ -4,15 +4,19 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 from get_link import send_mail, receive_mail, parse_link
 from settings import MAIN_CONFIG
+from loguru import logger
 
 TELEGRAM_KEY = MAIN_CONFIG['TELEGRAM_KEY']
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info('Got request from user')
     send_mail()
+    logger.info('Mail sent, waiting..')
     await asyncio.sleep(3)
     mail = receive_mail()
     hd_link = parse_link(mail)
+    logger.success('link was received')
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
